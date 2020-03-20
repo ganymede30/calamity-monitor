c4f62c6200aa8cef85577caba1b16df4ceba0ce7
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-20 14:32:40 -0400

    setup news

--- src/components/News.js
+++ src/components/News.js
@@ -1,18 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 import { NewsRenderer } from "./NewsRenderer";
+import axios from "axios";
 
-export const News = () => {
-  useEffect(() => {
+export default class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+
+  async componentDidMount() {
     const url =
       "http://newsapi.org/v2/top-headlines?apiKey=d9e78da03e4e419a881c2a2f9f1f746c&category=health&q=coronavirus";
-    fetch(url).then(article => {
-      console.log(article);
-    });
-  }, []);
 
-  return (
-    <div>
-      <NewsRenderer />
-    </div>
-  );
-};
+    const { data } = await axios.get(url);
+    this.setState({
+      news: data.articles
+    });
+  }
+
+  render() {
+    const { news } = this.state;
+    return (
+      <div>
+        <NewsRenderer articles={news} />
+      </div>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
-import { News } from "./components/News";
+import News from "./components/News";
 
 const Routes = () => {
   return (
