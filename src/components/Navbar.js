981021f87d68694d18f269e5711472996e909f6a
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2020-03-20 12:31:05 -0400

    created navbar component, as well as component foldoer

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link }  from 'react-router-dom'
+import {
+  AppBar,
+  Toolbar,
+  ListItem,
+  Button,
+  IconButton,
+  ListItemText,
+  Avatar,
+  Divider,
+  List,
+  Typography,
+  Box
+
+} from '@material-ui/core'
+
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+
+} from '@material-ui/icons'
+
+const Navbar = () => {
+    return (
+        <Box component= "nav">
+         <AppBar position= "static">
+             <Toolbar>
+            
+              <h1>Calamity Monitor</h1> 
+             </Toolbar>
+             <Button style={{color: "white"}}>
+               News
+             </Button>
+             <Button style={{color: "white"}}>
+                Health
+             </Button>
+         </AppBar>
+        </Box>
+    )
+}
+
+export default Navbar 
