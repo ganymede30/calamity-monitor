a7d2e89dfb52f497e9c9aa7426e5c7dbec62511f
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-20 23:02:20 -0400

    finished Api filtering for everything

--- src/apiFuncs.js
+++ src/apiFuncs.js
@@ -1,11 +1,26 @@
 import axios from "axios";
-
+import APIKey from "./secrets";
 export const baseUrl = "http://newsapi.org/v2/";
-export const everything = `${baseUrl}everything`;
-export const topHeadlines = `${baseUrl}top-headlines`;
+export const topHeadlines = `${baseUrl}top-headlines?apiKey=${APIKey}&q=coronavirus&pageSize=100`;
+
+export const getEverything = async (domains, language, sorting) => {
+  let endPoint = `${baseUrl}everything?apiKey=${APIKey}&q=coronavirus&pageSize=100`;
+  if (domains) endPoint += `&domains=${domains}`;
+  if (language) endPoint += `&language=${language}`;
+  if (sorting) endPoint += `&sorting=${sorting}`;
+
+  const result = await axios.get(endPoint).then(({ data }) => data);
+  return result;
+};
+
+export const getTopHeadlines = () => {};
+
+// setup category or q for corona virus everytime
 
 // if everything
+// params: domains, date(2020-03-21), language, sorting(relevancy, popularity, publishedAt(newer first))
 // function to extract filtered data, maybe a switch for the url. like a builder
 
 // top headlines
+// params: country(code), category(general, health, science, technology, sports, business),
 // filter by country, category
--- src/components/News.js
+++ src/components/News.js
@@ -1,28 +1,15 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { NewsRenderer } from "./NewsRenderer";
-import axios from "axios";
+import { getEverything } from "../apiFuncs";
 
-export default class News extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      news: []
-    };
-  }
+const News = () => {
+  const [news, setNews] = useState([]);
 
-  async componentDidMount() {
-    const url =
-      "http://newsapi.org/v2/everything?q=coronavirus&from=2020-03-20&pageSize=100&apiKey=d9e78da03e4e419a881c2a2f9f1f746c";
+  useEffect(() => {
+    getEverything("", "es", "publishedAt").then(({ articles }) => setNews(articles));
+  }, []);
 
-    const { data } = await axios.get(url);
-    this.setState({
-      news: data.articles
-    }); 
-  }
+  return news.map(article => <NewsRenderer article={article} key={article.url} />);
+};
 
-  render() {
-    return this.state.news.map(article => (
-      <NewsRenderer article={article} key={article.source.id} />
-    ));
-  }
-}
+export default News;
