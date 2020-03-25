33b7092961dd133dc207a8028cc1f60e64357e68
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-25 16:52:24 -0400

    cleaned clone a bit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,10 @@
 import React from "react";
 import World_Map from "./World_Map";
 
-import Slideview from "./Slideview";
-
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Slideview /> */}
-      <Map />
-=======
       <World_Map />
->>>>>>> master
     </div>
   );
 };
--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,7 @@ const News = () => {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    getTopHeadlines("", "health").then(articles => setNews(articles));
+    getTopHeadlines("us", "health").then(articles => setNews(articles));
   }, []);
   // [] = newsUpdate (true/false), will re-render if changed to true, setTimeout to change it. re renders everytime whats inside [] chnages
 
@@ -26,46 +26,5 @@ const News = () => {
     </>
   );
 };
-// filtering on the backend;
-// debounce axios
-// GET after clicking apply filters
-// store filters in state.
-// create a regular form with handleForm();
-// proxy server for the api requests.
-// front-end > proxy > api
-// axios hits router.get(route) > router.get hits api
-// create a blank model/class, this model has a class methods that hits the api
 
-// cloud firebase functions so our axios hits that and that hits the api
 export default News;
-
-// class News extends Component {
-//   state = {
-//     articles: []
-//   }
-
-//   componentDidMount () {
-//       fetch('/topHeadlines')
-//        .then(res => res.json())
-//        .then(articles => this.setState({ articles }))
-//   }
-
-//   render () {
-//     console.log('This is the state: ', this.state)
-
-//     return (
-//      <div>
-//         <h1>News</h1>
-//         <ul>
-//           {
-//             this.state.articles.map(article =>
-//             <li key={article.url} article={article}>{article.title}</li>
-//               )
-//           }
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// export default News
