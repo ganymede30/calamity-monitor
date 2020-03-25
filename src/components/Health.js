68769e5f05e1c3356538a8db93bc41a7de508a0d
Trevor <take1seet@gmail.com>
2020-03-25 17:05:42 -0400

    symptom info materialize

--- src/components/Health.js
+++ src/components/Health.js
@@ -9,11 +9,10 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import NavBar from './Navbar';
 import {loadCSS} from 'fg-loadcss';
 import Icon from '@material-ui/core/Icon';
+import CheckIcon from '@material-ui/icons/Check';
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -112,35 +111,58 @@ export default function Health() {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
+        <Typography variant="subtitle1">Be Overprepared</Typography>
+        <Typography paragraph>JIEJIFJIEFJIE</Typography>
+        <ListItem alignItems="flex-start">
+          <ListItemIcon>
+            <CheckIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  className={classes.inline}
+                  color="textPrimary"
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>{' '}
+        <Typography variant="h4">What is the Coronavirus(Covid-19)?</Typography>
+        <Typography variant="caption">
+          COVID-19 is a respiratory disease caused by a new coronavirus
+          (SARS-CoV-2) not previously seen in humans. An outbreak of COVID-19
+          began in late 2019 in Wuhan, a city in China’s Hubei province. To
+          date, cases of COVID-19 have spread around the world, making the
+          condition one of the most rapidly emerging infectious diseases.
+          COVID-19 can cause fever, cough, and shortness of breath. This virus
+          is spread through human contact, much like the cold or flu. There is
+          not yet a vaccine or medication approved to treat it. The virus has
+          been identified in over 100,000 people across the globe. While most
+          people recover from COVID-19, it can be life-threatening.
         </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Typography variant="h4">Symptoms to lookout for</Typography>
+        <ListItem alignItems="flex-start">
+          <ListItemIcon>
+            <CheckIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary="Fever, coughing, shortness of breath"
+            secondary={
+              <React.Fragment>
+                {
+                  ' — These symptoms may take up to 2-14 days to appear after exposure'
+                }
+              </React.Fragment>
+            }
+          />
+        </ListItem>
       </main>
     </div>
   );
