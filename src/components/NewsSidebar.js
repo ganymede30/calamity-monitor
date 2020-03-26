e8e7858ca4cfcbafa06520601fbb1f49fedc9388
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-26 10:42:28 -0400

    changed form to a hook

--- src/components/NewsSidebar.js
+++ src/components/NewsSidebar.js
@@ -1,5 +1,5 @@
 import FilterListIcon from "@material-ui/icons/FilterList";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Button,
   Dialog,
@@ -9,95 +9,92 @@ import {
   InputLabel,
   MenuItem,
   FormControl,
-  Select
+  Select,
+  makeStyles
 } from "@material-ui/core/";
 
-class DialogSelect extends Component {
-  constructor() {
-    super();
-    this.state = {
-      open: false,
-      country: "",
-      category: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClickOpen = this.handleClickOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  dialogTitle: {
+    paddingBottom: 0
   }
+}));
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+const DialogSelect = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [country, setCountry] = useState("");
+  const [category, setCategory] = useState("");
 
-  handleClickOpen() {
-    this.setState({ open: true });
-  }
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  handleClose() {
-    this.setState({ open: false });
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  render() {
-    return (
-      <div>
-        <Button color="inherit" onClick={this.handleClickOpen}>
-          <FilterListIcon />
-        </Button>
-        <Dialog
-          disableBackdropClick
-          disableEscapeKeyDown
-          open={this.state.open}
-          onClose={this.handleClose}>
-          <DialogTitle style={{ paddingBottom: 0 }}>Filter by: </DialogTitle>
-          <DialogContent>
-            <form style={{ display: "flex", flexWrap: "wrap" }}>
-              <FormControl style={{ margin: 20, minWidth: 120 }}>
-                <InputLabel htmlFor="demo-dialog-native">Country</InputLabel>
-                <Select
-                  name="country"
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={this.state.country}
-                  onChange={this.handleChange}>
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value="us">United States</MenuItem>
-                  <MenuItem value="ch">China</MenuItem>
-                  <MenuItem value="fr">France</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl style={{ margin: 20, minWidth: 120 }}>
-                <InputLabel htmlFor="demo-dialog-native">Category</InputLabel>
-                <Select
-                  name="category"
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={this.state.category}
-                  onChange={this.handleChange}>
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value="technology">Tech</MenuItem>
-                  <MenuItem value="business">Business</MenuItem>
-                  <MenuItem value="health">Health</MenuItem>
-                </Select>
-              </FormControl>
-            </form>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClose} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClose} color="primary">
-              Ok
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Button color="inherit" onClick={handleClickOpen}>
+        <FilterListIcon />
+      </Button>
+      <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle className={classes.dialogTitle}>Filter by: </DialogTitle>
+        <DialogContent>
+          <form className={classes.container}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="demo-dialog-native">Country</InputLabel>
+              <Select
+                name="country"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={country}
+                onChange={e => setCountry(e.target.value)}>
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="us">United States</MenuItem>
+                <MenuItem value="ch">China</MenuItem>
+                <MenuItem value="fr">France</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="demo-dialog-native">Category</InputLabel>
+              <Select
+                name="category"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={category}
+                onChange={e => setCategory(e.target.value)}>
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="technology">Tech</MenuItem>
+                <MenuItem value="business">Business</MenuItem>
+                <MenuItem value="health">Health</MenuItem>
+              </Select>
+            </FormControl>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
 export default DialogSelect;
