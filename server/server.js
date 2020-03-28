3e21c343171ab79506407955b55ba67ad18f4b0a
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-28 19:23:52 -0400

    test

--- server/server.js
+++ server/server.js
@@ -67,8 +67,6 @@ function errorHandler(error, req, res, next) {
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log("Listening on port ", port);
 });
