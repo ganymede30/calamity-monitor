8b4808148b2b1b3fde3a4f4fcad77053288c20af
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-29 00:17:59 -0400

    la

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ app.use(morgan());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
 
