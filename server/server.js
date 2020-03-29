4316dc8311c5e7c6783105709b3a98c9a050b5a6
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-29 00:10:14 -0400

    s

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const fetch = require("node-fetch");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const path = require("path");
 
 require("dotenv").config();
