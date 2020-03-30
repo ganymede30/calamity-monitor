bdf994ff22ede5c81289fa11a896d9f9d4a4a5ad
Michael Kenny <mfkenny130@gmail.com>
2020-03-30 00:48:26 -0400

    Rough attempt at cedar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WorldMap from "./WorldMap";
 import LoadScreen from "./LoadScreen";
+import LineGraph from "./Maps/LineGraph"
 
 const Home = () => {
   return (
     <>
       <LoadScreen />
+      <LineGraph />
       <WorldMap />
     </>
   );
