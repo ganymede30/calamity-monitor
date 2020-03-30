b4f48ae246595137d8d8894f204772f1358f9057
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-30 13:00:27 -0400

    added message at the bottom of news

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getTopHeadlines } from "../services/apiFuncs";
-import { Wrapper, Title, SubWrapper } from "../styles/newsStyles";
+import { Wrapper, Title, SubWrapper, Text } from "../styles/newsStyles";
 import List from "./List";
 import { useInfiniteScroll } from "../utils/hooks/useInfiniteScroll";
 import DialogSelect from "./NewsSidebar";
@@ -21,6 +21,11 @@ const News = () => {
           <DialogSelect setNews={setNews} />
         </SubWrapper>
         <List articles={news.slice(0, count)} />
+        {news.length <= count ? (
+          <Text>No more articles at this time.</Text>
+        ) : (
+          <Text>Loading more news.</Text>
+        )}
       </Wrapper>
     </>
   );
