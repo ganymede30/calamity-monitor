8b362ec72b558bdee6edc3356907681f16a995a7
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-31 13:54:26 -0400

    responsive navbar working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,11 @@
-import React, {useState} from "react";
-import { Grid, AppBar, Button, Typography, Box, IconButton} from "@material-ui/core";
+import React from "react";
+import { Grid, AppBar, Button, Typography, Box, IconButton } from "@material-ui/core";
 import { Brightness4, Brightness7 } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/styles";
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-import MobileNav from './MobileNav'
-
-
-import styled from "styled-components"
-import { mobile } from "../styles/mediaQueries"
-
-
+import MobileNav from "./MobileNav";
+import { mobile } from "../styles/mediaQueries";
 
 const darkMode = makeStyles(() => {
   return {
@@ -19,10 +14,11 @@ const darkMode = makeStyles(() => {
       color: "#e0e0e0"
     },
     navBar: {
-      backgroundColor: "#333333"
+      backgroundColor: "#333333",
+      padding: "0 1%"
     },
     gridContainer: {
-      height: "65px"
+      height: "55px"
     }
   };
 });
@@ -33,31 +29,25 @@ const lightMode = makeStyles(() => ({
     color: "#fafafa"
   },
   navBar: {
-    backgroundColor: "primary"
+    backgroundColor: "primary",
+    padding: "0 1%"
   },
   gridContainer: {
-    height: "65px"
+    height: "55px"
   }
 }));
-
-
-
 
 const Navbar = ({ theme, setTheme }) => {
   let classes;
   theme ? (classes = darkMode()) : (classes = lightMode());
-  console.log("This is the window: ", window)
-  const matches = useMediaQuery('(max-width: 480px)')
-  
-  return ( 
-  
-    matches ? 
-    <MobileNav theme={theme} setTheme={setTheme}/> 
-    : 
- 
-   <Box component="nav">
-     <AppBar position="static" className={classes.navBar}>
-       <Grid
+  const matches = useMediaQuery(mobile);
+
+  return matches ? (
+    <MobileNav theme={theme} setTheme={setTheme} />
+  ) : (
+    <Box component="nav">
+      <AppBar position="static" className={classes.navBar}>
+        <Grid
           className={classes.gridContainer}
           container
           justify="space-between"
@@ -67,27 +57,26 @@ const Navbar = ({ theme, setTheme }) => {
             <Button href="/">
               <Typography className={classes.buttonText}>Calamity Monitor</Typography>
             </Button>
-          </Grid> 
+          </Grid>
           <Grid item>
-        <Button href="/news">
+            <Button href="/news">
               <Typography className={classes.buttonText}>News</Typography>
             </Button>
-        <Button href="/health">
+            <Button href="/health">
               <Typography className={classes.buttonText}>Health</Typography>
             </Button>
-          <IconButton
+            <IconButton
               className={classes.buttonText}
               title="Toggle light/dark theme"
               aria-label="Toggle light/dark theme"
               onClick={() => setTheme()}>
               {theme ? <Brightness7 /> : <Brightness4 />}
             </IconButton>
-       </Grid> 
-     </Grid>
-        </AppBar>
-</Box>
-
- );
+          </Grid>
+        </Grid>
+      </AppBar>
+    </Box>
+  );
 };
 
-export default Navbar
+export default Navbar;
--- src/styles/global.js
+++ src/styles/global.js
@@ -10,8 +10,7 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: Lato, Helvetica, Roboto, sans-serif;
-    width: 100vw;
-    overflow-x: hidden;
+    width: 100%;
     background: ${({ theme }) => theme.backgroundColor};
   }
 
@@ -28,12 +27,12 @@ const GlobalStyles = createGlobalStyle`
   }
 
   .webmap {
-    height: Calc(100vh - 9vh);
+    height: Calc(100vh - 10vh);
   }
 
   ${mobile} {
     .webmap {
-      height: Calc(90vh - 10vh)
+      height: Calc(90vh - 8vh)
     }
   }
 `;
