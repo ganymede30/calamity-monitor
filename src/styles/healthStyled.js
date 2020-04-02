e44747cb6c5b76c0d8c1b36c80f2c77e8ac4d32a
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-04-02 14:28:48 -0400

    first layer

---
+++ src/styles/healthStyled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { tablet } from "./mediaQueries";
+
+export const Title = styled.h1`
+  color: ${props => props.theme.titleText};
+  font-size: 1.5em;
+  font-weight: 500;
+  margin-bottom: 0;
+  margin-top: 3px;
+`;
